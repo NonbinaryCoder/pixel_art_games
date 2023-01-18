0a9c3896d0f3aa9ed00095d74957b1fdb93d5355
NonbinaryCoder <nonbinarycoderdeveloping@gmail.com>
2023-01-17 19:57:51 -0500

    Escape now exits game

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use iyes_loopless::prelude::*;
+
+use crate::{input::EXIT_KEYS, GameState};
 
 mod appear_test;
 
@@ -6,7 +9,8 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(appear_test::AppearTestPlugin);
+        app.add_plugin(appear_test::AppearTestPlugin)
+            .add_system(exit_game_system.run_if_not(GameState::current_is_menu));
     }
 }
 
@@ -15,4 +19,10 @@ pub enum GameType {
     #[default]
     AppearTest,
     Cart,
+}
+
+fn exit_game_system(mut commands: Commands, keys: Res<Input<KeyCode>>) {
+    if keys.any_just_pressed(EXIT_KEYS) {
+        commands.insert_resource(NextState(GameState::MainMenu));
+    }
 }
--- src/input.rs
+++ src/input.rs
@@ -7,3 +7,5 @@ pub const FORWARD_KEYS: [KeyCode; 5] = [
     KeyCode::Up,
     KeyCode::Right,
 ];
+
+pub const EXIT_KEYS: [KeyCode; 1] = [KeyCode::Escape];
